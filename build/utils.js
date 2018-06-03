22ecf2f8ff1cfee6c8f3fc8ab6d86c587f7d1389
gm7777777 <119618949@qq.com>
2018-06-03 17:31:47 +0800

    first commit

---
+++ build/utils.js
@@ -0,0 +1,161 @@
+'use strict'
+const path = require('path')
+const config = require('../config')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const packageConfig = require('../package.json')
+
+exports.assetsPath = function (_path) {
+  const assetsSubDirectory = process.env.NODE_ENV === 'production'
+    ? config.build.assetsSubDirectory
+    : config.dev.assetsSubDirectory
+
+  return path.posix.join(assetsSubDirectory, _path)
+}
+
+exports.cssLoaders = function (options) {
+  options = options || {}
+
+  const cssLoader = {
+    loader: 'css-loader',
+    options: {
+      sourceMap: options.sourceMap
+    }
+  }
+
+  const postcssLoader = {
+    loader: 'postcss-loader',
+    options: {
+      sourceMap: options.sourceMap
+    }
+  }
+
+  // generate loader string to be used with extract text plugin
+  function generateLoaders (loader, loaderOptions) {
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
+
+    if (loader) {
+      loaders.push({
+        loader: loader + '-loader',
+        options: Object.assign({}, loaderOptions, {
+          sourceMap: options.sourceMap
+        })
+      })
+    }
+
+    // Extract CSS when that option is specified
+    // (which is the case during production build)
+    if (options.extract) {
+      return ExtractTextPlugin.extract({
+        use: loaders,
+        fallback: 'vue-style-loader'
+      })
+    } else {
+      return ['vue-style-loader'].concat(loaders)
+    }
+  }
+
+  // https://vue-loader.vuejs.org/en/configurations/extract-css.html
+  return {
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    less: generateLoaders('less'),
+    sass: generateLoaders('sass', { indentedSyntax: true }),
+    scss: generateLoaders('sass'),
+    stylus: generateLoaders('stylus'),
+    styl: generateLoaders('stylus')
+  }
+}
+
+// Generate loaders for standalone style files (outside of .vue)
+exports.styleLoaders = function (options) {
+  const output = []
+  const loaders = exports.cssLoaders(options)
+
+  for (const extension in loaders) {
+    const loader = loaders[extension]
+    output.push({
+      test: new RegExp('\\.' + extension + '$'),
+      use: loader
+    })
+  }
+
+  return output
+}
+
+exports.createNotifierCallback = () => {
+  const notifier = require('node-notifier')
+
+  return (severity, errors) => {
+    if (severity !== 'error') return
+
+    const error = errors[0]
+    const filename = error.file && error.file.split('!').pop()
+
+    notifier.notify({
+      title: packageConfig.name,
+      message: severity + ': ' + error.name,
+      subtitle: filename || '',
+      icon: path.join(__dirname, 'logo.png')
+    })
+  }
+}
+
+var glob = require('glob');
+
+exports.getResEntries = (globPath) => {
+  var entries = {},
+    basename, tmp, pathname;
+  if (!(globPath instanceof Array)&&typeof (globPath) != "object") {
+    globPath = [globPath]
+  }
+  globPath.forEach((itemPath) => {
+    glob.sync(itemPath).forEach(function (entry) {
+      basename = path.basename(entry, path.extname(entry));
+      if (entry.split('/').length > 4) {
+        tmp = entry.split('/').splice(-3);
+        pathname = tmp.splice(0, 1) + '/' + basename; // 正确输出js和html的路径
+        entries[pathname] = entry;
+      } else {
+        entries[basename] = entry;
+      }
+    });
+  });
+  return entries;
+}
+
+exports.getWebpackPlugins =(globPath,jsEntries) => {
+  var webpackplugins = [],
+    basename, tmp, pathname;
+  if (!(globPath instanceof Array)&&typeof (globPath) != "object") {
+    globPath = [globPath]
+  }
+  globPath.forEach((itemPath) => {
+    glob.sync(itemPath).forEach(function (entry) {
+      basename = path.basename(entry, path.extname(entry));
+      if (entry.split('/').length > 4) {
+        tmp = entry.split('/').splice(-3);
+        basename = tmp.splice(0, 1) + '/' + basename; // 正确输出js和html的路径
+        // entries[pathname] = entry;
+      } else {
+        // entries[basename] = entry;
+      }
+
+      // 配置生成的html文件，定义路径等
+      var conf = {
+        filename: basename + '.html',
+        template: entry,   // 模板路径
+        inject: true,              // js插入位置
+        // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+        chunksSortMode: 'dependency'
+      };
+
+      if (basename in jsEntries) {
+        conf.chunks = ['manifest', 'vendor', basename];
+        conf.hash = true;
+      }
+      // webpackplugins.push(new HtmlWebpackPlugin(conf));
+      webpackplugins.push(conf);
+    });
+  });
+  return webpackplugins;
+}
---
+++ src/pages/index/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import BaseLayout from '@/components/BaseLayout'
+import BaseLayout2 from '@/components/BaseLayout2'
+/* eslint-disable no-new */
+new Vue({
+  el: '#index',
+  data: {
+    show: true
+  },
+  components: { BaseLayout }
+})
